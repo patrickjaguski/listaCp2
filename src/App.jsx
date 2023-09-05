6a1e1e7768b4cb6bcf5152259b22b110f49deb4e
patrickjaguski <patrick_jaguski@hotmail.com>
2023-09-05 20:51:43 -0300

    links ajustado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import Menu from "../../Components/Menu"
-import Rodape from "../../Components/Rodape"
+import Menu from "./Components/Menu"
+import Rodape from "./Components/Rodape"
 
 function App() {
 
