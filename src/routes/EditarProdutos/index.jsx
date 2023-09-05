44f002855b6759c6b03310b8c95026b0a9fdce15
patrickjaguski <patrick_jaguski@hotmail.com>
2023-09-05 19:43:37 -0300

    Indexs e componentes criados

---
+++ src/routes/EditarProdutos/index.jsx
@@ -0,0 +1,21 @@
+import { useParams, useNavigate } from "react-router-dom"
+import { listaProdutos } from "../../components/listaProdutos.js"
+export default function EditarProdutos(){
+
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+
+    const salvar = ()=>{
+
+        alert(`Prdouto:${produto.nome} editado com sucesso!`)
+        return navegacao(`/produtos`)
+    }
+    
+    return(
+        <main>
+            <h1>Editando Produtos</h1>
+            <p>Editando os dados do produto: {Produtos.nome}</p>
+            <button onClick={salvar}>Salvar</button>
+        </main>
+    )
+}
